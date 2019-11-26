9f9e97b8088ae0caef6edd148906289446253822
Abdul Waheed <bawan@abacusoft.com>
2019-11-26 22:27:58 +0500

    Static broadcast receiver

---
+++ app/src/main/java/com/abdul_waheed/serviceandbackgroundtask/ExampleBroadcastReceiver.java
@@ -0,0 +1,22 @@
+package com.abdul_waheed.serviceandbackgroundtask;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
+
+public class ExampleBroadcastReceiver extends BroadcastReceiver {
+    public static final String TAG = ExampleBroadcastReceiver.class.getSimpleName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Toast.makeText(context, "Boot Completed",Toast.LENGTH_SHORT).show();
+        }
+
+        if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
+            Toast.makeText(context, "Connectivity Changed", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
